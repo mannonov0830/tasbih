7f190ee16947a7e4e955ccdce56dd7f7b27ffc89
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-04-13 00:20:39 +0500

    main

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".dropbtn");
+const menu = document.querySelector(".dropdown-content");
+
+btn.addEventListener("click", () => {
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } 
+    else {
+        menu.style.display = "block";
+    }
+
+});
