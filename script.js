38c183f071a3e8587fb139aab1ce7ed84f1b6bd2
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-04-13 08:30:35 +0500

    main

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ btn.addEventListener("click", () => {
 
     if (menu.style.display === "block") {
         menu.style.display = "none";
-    } 
+    }
     else {
         menu.style.display = "block";
     }
 });
+
+let count = 0
+let step = 1
+
+let btndrop = document.getElementById("btnDown")
+let btnEdit = document.querySelector(".btn_edit")
+let btnReset = document.querySelector(".btn_reset")
+let sum = document.querySelector(".sum h4")
+let btnTop = document.querySelector(".btn_top")
+let btnBotton = document.querySelector(".btn_botton")
+
+
+
